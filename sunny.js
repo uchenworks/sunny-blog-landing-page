5288dfb97dbe0ed6994136618e9c9d0692c14906
Uche <125195781+uchenworks@users.noreply.github.com>
2023-08-09 17:48:36 +0100

    Add files via upload
    A landing page for an agency

---
+++ sunny.js
@@ -0,0 +1,11 @@
+function menuFunction() {
+  document.getElementById("navbar").style.display = "block";
+  document.getElementById("cancel-bar").style.display = "block";
+  document.getElementById("menubar").style.display = "none";
+}
+
+function cancelFunction() {
+  document.getElementById("navbar").style.display = "none";
+  document.getElementById("cancel-bar").style.display = "none";
+  document.getElementById("menubar").style.display = "block";
+}
